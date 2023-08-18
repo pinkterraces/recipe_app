8c243305b17209c1ee94a9020948a6302f726e5a
pinkterraces <108399126+pinkterraces@users.noreply.github.com>
2023-08-18 19:11:20 +0200

    Implemented Authentications

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   #to display lists and details
-from .models import Recipe       
+from .models import Recipe
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView):                           #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):                           #class-based view
    model = Recipe                                         #specify model
-   template_name = 'recipes/recipes_home.html'                    #specify template
+   template_name = 'recipes/recipes.html'                 #specify template
 
-class RecipeDetailView(DetailView):                       #class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):                       #class-based view
    model = Recipe                                        #specify model
    template_name = 'recipes/detail.html' 
